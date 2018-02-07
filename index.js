810e9d7e0586d137357332ab3b6a9b8ad3a4023d
Stepan Sendun <Stepan_Sendun@epam.com>
2018-02-07 12:30:51 +0200

    added started pattern

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 //https://jsonplaceholder.typicode.com/posts
-let interval;
-const start = document.querySelector('.start');
-const stop = document.querySelector('.stop');
 
-const myObservable = new Rx.Subject();
-
-myObservable.subscribe(post => console.log(post.title))
-
-function startFetching() {
-    let i = 1;
-    interval = Rx.Observable.interval(2000).subscribe(
-        v => {
-            fetch(`https://jsonplaceholder.typicode.com/posts/${i}`)
-                .then(response => response.json())
-                .then(post => {
-                    i++;
-                    myObservable.next(post);
-                })
-        }
-    );
-}
-
-function stopFetching() {
-    interval.complete();
-}
-
-start.addEventListener('click', startFetching);
-stop.addEventListener('click', stopFetching);
+const container = document.querySelector('.container-fluid');
+const first = document.querySelector('.first');
