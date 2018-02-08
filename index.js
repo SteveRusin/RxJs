965a4b1d86760551eca7abbd9d88665b440d2c05
Steve <steve.neeson21@gmail.com>
2018-02-08 22:18:52 +0200

    rewritten task1

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-Rx.Observable.fromPromise(fetch('https://jsonplaceholder.typicode.com/users'))
-    .subscribe(
-    (response) => {
-        Rx.Observable.from(response.json())
-            .map(users => users.map(user => user.name))
-            .subscribe(
-            result => console.log(result),
-        );
+const { fromPromise } = Rx.Observable;
+
+const subscription = fromPromise(fetch('https://jsonplaceholder.typicode.com/users'))
+    .flatMap(res => res.json())
+    .flatMap(res => Rx.Observable.from(res.map(user => user.name)))
+
+
+
+
+subscription.subscribe(
+    (users) => {
+        console.log(users)
     },
     (err) => console.error(err)
-    )
+)
