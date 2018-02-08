986ba008fe8ede74fd8a6af760de14ef2fe2c860
Steve <steve.neeson21@gmail.com>
2018-02-08 23:37:02 +0200

    map return just value without transformation

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+const { of, create, interval, timeInterval } = Rx.Observable;
+
+Rx.Observable.prototype.map = function (source) {
+    return this;
+
+}
+
+const subscribtion = interval(1000)
+    .take(Infinity)
+    .map(x => x)
+
+
+subscribtion.subscribe(data => {
+    console.log(data)
+})
