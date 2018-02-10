433a5f1942df57c292ab1e19caac654d2b91f88d
Steve <steve.neeson21@gmail.com>
2018-02-10 18:15:54 +0200

    done task4

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 
 const { of, create, interval, timeInterval } = Rx.Observable;
 
-Rx.Observable.prototype.map = function (source) {
-    return this;
+Rx.Observable.prototype.map = function (fn) {
+    const source = this;
 
+    return create(observer => {
+        return source.subscribe(src => {
+            observer.next(fn(src))
+        })
+    })
+}
+
+function addOne(x) {
+    return x + 1;
 }
 
 const subscribtion = interval(1000)
     .take(Infinity)
-    .map(x => x)
+    .map(x => x+5)
 
 
 subscribtion.subscribe(data => {
